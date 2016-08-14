8fa6e52700293ce5001cac621b0bd93ee07186a6
tophatraptor <jsdhanoa987@gmail.com>
2016-08-14 11:32:23 -0500

    initial commit, \usepackage not working

---
+++ mse.py
@@ -0,0 +1,77 @@
+import os
+import glob
+import sys
+import multiprocessing as mp
+import re
+from collections import Counter
+import subprocess
+from subprocess import PIPE
+from core.funcs import *
+
+def mse(filename):
+    global outpath
+    global path
+    cleanname = os.path.splitext(os.path.basename(filename))[0]
+    with open(filename, mode='r', encoding='latin-1') as fh:
+        text = fh.read()
+    text = re.sub(r'(?m)^%+.*$', '', text)
+    text = re.sub(r"(?m)([^\\])\%+.*?$", r'\1', text)
+    text = re.sub(r'\\begin\{comment\}.*?\\end\{comment\}','',text,re.DOTALL)
+    packages = re.findall(r'\\usepackage(?:\[.*?\])?\{.*?\}',text)
+    packages = '\n'.join(packages)
+    stylefile = os.path.join(outpath,cleanname+'.sty')
+    imgname = os.path.join(outpath,cleanname+'.png')
+    with open(stylefile,'w') as fh:
+        fh.write(packages)
+    eqlabels = []
+    occurrences = []
+    equations = re.findall(r'(?s)\\begin\{equation\}.*?\\end\{equation\}|\\begin\{multline\}.*?\\end\{multline\}|\\begin\{gather\}.*?\\end\{gather\}|\\begin\{align\}.*?\\end\{align\}|\\begin\{flalign\*\}.*?\\end\{flalign\*\}|\\begin\{math\}.*?\\end\{math\}|[^\\]\\\[.*?\\\]|\$\$[^\^].*?\$\$',text)
+    if len(equations)==0:
+        return(-1)
+    for equation in equations:
+        x = re.match(r'\\label\{(.*?)\}',equation)
+        if x:
+            eqlabels.append(x.group(1).strip())
+    labels = re.findall(r'\\eqref\{(.*?)\}',text) + re.findall(r'\\ref\{(.*?)\}',text)
+    for label in labels:
+        if label.strip() in eqlabels:
+            occurrences.append(label.strip())
+    count = Counter(occurrences).most_common(1)
+    if count:
+        if count[0][1]==1:
+            rendereq = equations[0]
+        else:
+            rendereq = count[0][0]
+    else:
+        rendereq = equations[0]
+    print("{}: Rendereq: {}".format(filename,rendereq))
+    print("Outputting to: {}".format(imgname))
+    proc = subprocess.Popen(["latexmlmath", "--preload="+stylefile, "--mathimage=" + imgname,"-"])
+    stdout, stderr = proc.communicate(rendereq)
+    print("stdout: {}".format(stdout))
+    print("stderr: {}".format(stderr))
+    print("Finished: {}".format(filename))
+    return 0
+
+def main():
+    global outpath
+    global path
+    if len(sys.argv)>3:
+        print("Error: require input/output directories", file=sys.stderr)
+        sys.exit()
+    path = os.path.join(sys.argv[1],'')
+    outpath = os.path.join(sys.argv[2],'')
+    if not os.path.isdir(path):
+        print("Error: invalid input directory")
+        sys.exit()
+    if not os.path.isdir(outpath):
+        os.makedirs(outpath)
+    pool = mp.Pool(processes=mp.cpu_count())
+    doclist = getmathfiles(path)
+    doclist = map(os.path.abspath,doclist)
+    pool.map(mse,doclist)
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+    main()
