b54b913d3d67ebf7f23cdfcbbb46cbf74763c1c8
tophatraptor <jsdhanoa987@gmail.com>
2016-08-21 15:01:00 -0500

    refactored & standardized regex

--- core/funcs.py
+++ core/funcs.py
@@ -8,20 +8,30 @@ def gettexfiles(path):
     file_list = glob.glob(os.path.join(absolute_path,'*.tex'))
     return file_list
 
+def grabmath(text, split=0):
+    delim = r'|'
+    a = r'\\begin\{equation\*?\}.*?\\end\{equation\*?\}'
+    b = r'\\begin\{multline\*?\}.*?\\end\{multline\*?\}'
+    c = r'\\begin\{gather\*?\}.*?\\end\{gather\*?\}'
+    d = r'\\begin\{align\*?\}.*?\\end\{align\*?\}'
+    e = r'\\begin\{flalign\*?\}.*?\\end\{flalign\*?\}'
+    f = r'\\begin\{math\*?\}.*?\\end\{math\*?\}'
+    g = r'[^\\]\\\[.*?\\\]'
+    h = r'\$\$[^\^].*?\$\$'
+    exprmatch = [a,b,c,d,e,f,g,h]
+    if(split):
+        tomatch = r'(?s)('+delim.join(exprmatch)+r')'
+        matches = re.split(tomatch,text)
+        return matches
+    else:
+        tomatch = r'(?s)' + delim.join(exprmatch)
+        matches = re.findall(tomatch,text)
+        return matches
+
 def hasmath(filename):
     with open(filename, mode='r', encoding='latin-1') as f1:
         text = f1.read()
-    a = re.findall(r'\\begin\{equation\}(.*?)\\end\{equation\}',text,re.DOTALL)
-    b = re.findall(r'\\begin\{multline\}(.*?)\\end\{multline\}',text,re.DOTALL)
-    c = re.findall(r'\\begin\{gather\}(.*?)\\end\{gather\}',text,re.DOTALL)
-    d = re.findall(r'\\begin\{align\}(.*?)\\end\{align\}',text,re.DOTALL)
-    e = re.findall(r'\\begin\{flalign\*\}(.*?)\\end\{flalign\*\}',text,re.DOTALL)
-    f = re.findall(r'\\begin\{math\}(.*?)\\end\{math\}',text,re.DOTALL)
-    g = re.findall(r'[^\\]\\\[(.*?)\\\]',text,re.DOTALL)
-    h = re.findall(r'\$\$([^\^].*?)\$\$',text,re.DOTALL)
-    l = re.findall(r'[^\\]\$(.*?)\$',text,re.DOTALL)
-    m = re.findall(r'\\\((.*?)\\\)',text,re.DOTALL)
-    finds = a + b + c + d + e + f + g + h
+    finds = grabmath(text)
     return (filename, len(finds))
 
 def getmathfiles(path):
