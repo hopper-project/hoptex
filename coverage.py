b6ff0b407f798a9f45488b2aa1078f4ebbf3034c
tophatraptor <jsdhanoa987@gmail.com>
2016-09-08 13:58:46 -0500

    updated coverage script

--- coverage.py
+++ coverage.py
@@ -3,11 +3,11 @@ import glob
 import sys
 import multiprocessing as mp
 import re
-
+from core.funcs import *
 def eqerrors(filename):
     with open(filename) as fh:
         text = fh.read()
-    equations = re.findall(r'\<math.*?\<\/math>',text)
+    equations = re.findall(r'(?s)\<table.*?\<\/table>',text)
     partialerror = 0
     for eq in equations:
         if "ltx_ERROR" in eq:
@@ -21,33 +21,33 @@ def main():
     if len(sys.argv)==1:
         print("Error: must pass in a log or logs", file=sys.stderr)
         sys.exit()
-    for x in sys.argv[1:]:
-        if not os.path.isfile(x):
-            print("Error: {} is not a valid file".format(x),file=sys.stderr)
-            continue
-        lines = []
-        timed_out = []
-        failed = []
-        with open(x) as fh:
-            lines = fh.readlines()
-        for line in lines:
-            ex = line.strip().split(':')
-            if len(ex)==2:
-                if ex[1]==timeouterror:
-                    timed_out.append(ex[0])
-                elif ex[1]==conversionerror:
-                    failed.append(ex[0])
-        print("Failed: {}".format(len(failed)))
-        print("Timed out: {}".format(len(timed_out)))
-        outputdir = x[:-4] + '/'
-        outputtedfiles = glob.glob(os.path.join(outputdir,'*.xhtml'))
-        pool = mp.Pool(processes=mp.cpu_count())
-        coverage = pool.map(eqerrors,outputtedfiles)
-        errors, total = zip(*coverage)
-        erroreqs = sum(errors)
-        totaleqs = sum(total)
-        print("{}/{} equations encountered nonfatal errors".format(erroreqs,totaleqs))
-        print("{}% total coverage".format((1-erroreqs/totaleqs)*100))
+    outputdir = sys.argv[3]
+    path = sys.argv[2]
+    log = sys.argv[1]
+    lines = []
+    timed_out = []
+    failed = []
+    with open(sys.argv[1]) as fh:
+        lines = fh.readlines()
+    for line in lines:
+        ex = line.strip().split(':')
+        if len(ex)==2:
+            if ex[1]==timeouterror:
+                timed_out.append(ex[0])
+            elif ex[1]==conversionerror:
+                failed.append(ex[0])
+    print("Failed: {}".format(len(failed)))
+    print("Timed out: {}".format(len(timed_out)))
+    total_failures = len(failed)+len(timed_out)
+    xhtmlfilelist = glob.glob(os.path.join(outputdir,'*.xhtml'))
+    mathfilelist = getmathfiles(path)
+    pool = mp.Pool(processes=mp.cpu_count())
+    coverage = pool.map(eqerrors,xhtmlfilelist)
+    errors, total = zip(*coverage)
+    erroreqs = sum(errors)
+    totaleqs = sum(total)
+    print("{}% nonfatal error equation coverage".format(round((1-erroreqs/totaleqs)*100,2)))
+    print("{}% document coverage".format(round((1-total_failures/len(mathfilelist))*100,2)))
 
 
 if __name__ == '__main__':
