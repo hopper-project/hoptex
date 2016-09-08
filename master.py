da0a4812222283b18fb06212aca77551f713c2e9
tophatraptor <jsdhanoa987@gmail.com>
2016-09-08 09:10:15 -0500

    added master script

---
+++ master.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import subprocess
+
+
+def main():
+    if len(sys.argv)!=4:
+        print("Usage: python3 master.py /path/to/dir/of/tex/dirs/ /path/to/xhtml/output/ /path/to/json/output/")
+        exit()
+    path = sys.argv[1]
+    xhtmlpath = sys.argv[2]
+    jsonpath = sys.argv[3]
+    cdir = os.listdir(path)
+    outfolders = []
+    folders = []
+    for x in cdir:
+        if os.path.isdir(x) and x[0]!='.':
+            outfolders.append(os.path.split(os.path.abspath(x))[1])
+            folders.append(os.path.abspath(x))
+
+    xhtml = []
+    json = []
+    for x in outfolders:
+        xhtml.append(os.path.join(xhtmlpath,x+'_converted/'))
+        json.append(os.path.join(jsonpath,x+'_json/'))
+
+    for i, x in enumerate(outfolders):
+        # print(outfolders[i],xhtml[i],json[i])
+        proc = subprocess.Popen(["python3","convertlatex.py",outfolders[i],xhtml[i]])
+        proc.wait()
+        proc = subprocess.Popen(["python3","proctex.py",outfolders[i],xhtml[i],json[i]])
+        proc.wait()
+
+if __name__=='main':
+    main()
