d36c46e6dd618166b39ea11630a3814adaa341bc
tophatraptor <jsdhanoa987@gmail.com>
2017-02-27 23:31:18 -0600

    added ability to render subset of tsv from eqid list

--- renderimage.py
+++ renderimage.py
@@ -23,12 +23,18 @@ def sqlgrab(filename):
 def tsvgrab(filename):
     print("Loading TSV...")
     global outpath
+    global subset
     outlist = []
     with open(filename,'r') as fh:
         for line in fh:
             eqid, eqtext = line.split('\t')
-            eqtext = eqtext.encode().decode('unicode_escape')
-            outlist.append(cleantuple((eqid, eqtext)))
+            eqtext = eqtext.encode().decode('unicode_escape').strip()
+            if subset:
+                if eqid.lower() in subset:
+                    outlist.append(cleantuple((eqid, eqtext)))
+            else:
+                outlist.append(cleantuple((eqid, eqtext)))
+    print("Loaded {} equations".format(len(outlist)))
     return(outlist)
 
 def render(tup):
@@ -61,16 +67,24 @@ def render(tup):
 def main():
     global outpath
     global stylefile
+    global subset
     parser = argparse.ArgumentParser(description='Options for rendering LaTeX images of files')
     parser.add_argument('--sql',action='store_true', help="Use when passing in a sqlite.out file")
     parser.add_argument('--tsv', action='store_true', help="Use when passing in a tsv of the format EQID formula")
     parser.add_argument('--sty', help="Optional parameter for .sty file for use with latexmlmath")
+    parser.add_argument("--eqidlist",help="Optional parameter for text file with list of equation ids")
     parser.add_argument("fname",help="Input file/folder")
     parser.add_argument("outdir",help="Path to output file/folder")
     args = parser.parse_args()
     fname = args.fname
     outpath = os.path.abspath(args.outdir)
+    validate_folder(outpath)
     stylefile = args.sty
+    if args.eqidlist:
+        with open(args.eqidlist,mode='r',encoding='latin-1') as fh:
+            subset = set(fh.read().split('\n'))
+    else:
+        subset = set()
     if(stylefile):
         stylefile = os.path.abspath(stylefile)
     pool = mp.Pool(processes=mp.cpu_count())
