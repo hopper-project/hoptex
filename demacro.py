40b08510d1179b16d9dcc7fbfbc37b778afbc9c4
tophatraptor <jsdhanoa987@gmail.com>
2017-03-02 21:52:58 -0600

    reduced timeout to 180s

--- demacro.py
+++ demacro.py
@@ -818,7 +818,7 @@ def demacro_file(path):
     start_time = time.time()
     text = load_inputs(path)
     newlines  = len(re.findall(r'\n',text))
-    timeout = 240
+    timeout = 180
     macrodict = {}
     new_macros = True
     changed = True
