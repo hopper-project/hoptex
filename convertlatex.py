1dc2520f8537e2d4c6f42e31d86748d85e476a86
tophatraptor <jsdhanoa987@gmail.com>
2017-04-17 17:05:28 -0500

    further timeout features

--- convertlatex.py
+++ convertlatex.py
@@ -1,6 +1,7 @@
 """Script for converting a folder of .tex documents to a folder of
 .xhtml documents, using LaTeXML
 """
+
 import sys #handling arguments passed to function
 import glob #file path handling
 import os #checking files and writing to/from files
@@ -13,9 +14,13 @@ import datetime
 from subprocess import PIPE
 from core.funcs import *
 
+global timeout
+global erroroutputpath
+global outpath
+
 path = ''
 outpath = ''
-
+timeout = 240
 def writesanitized(sanitized, clean_file_name):
     """Writes sanitized document text to clean_file_name"""
     global erroroutputpath
@@ -26,8 +31,12 @@ def writesanitized(sanitized, clean_file_name):
 def genxhtml(filename):
     """Conversion function using subprocess"""
     global outpath
+    global timeout
     clean_file_name = os.path.splitext(os.path.basename(filename))[0]
     outfname = outpath + clean_file_name+'.xhtml'
+    outrawname = output+clean_file_name+'.txt'
+    outdir = outpath + clean_file_name
+    validate_folder(outdir)
     if os.path.isfile(outfname):
         # print("{}: Already generated".format(filename))
         return ""
@@ -41,7 +50,7 @@ def genxhtml(filename):
     try:
         proc = subprocess.Popen(["latexml", "-"], stderr=PIPE, stdout=PIPE,
         stdin=PIPE)
-        stdout, stderr = proc.communicate(output.encode(), timeout=60)
+        stdout, stderr = proc.communicate(output.encode(), timeout=timeout)
     except subprocess.TimeoutExpired:
         proc.kill()
         print("{}: MathML conversion failed - timeout".format(filename))
@@ -54,12 +63,21 @@ def genxhtml(filename):
     try:
         proc = subprocess.Popen(["latexmlpost", "--format=xhtml", "-"],
         stderr=PIPE, stdout=PIPE, stdin=PIPE)
-        stdout2, stderr = proc.communicate(stdout, timeout=60)
+        stdout2, stderr = proc.communicate(stdout, timeout=timeout)
     except subprocess.TimeoutExpired:
         proc.kill()
         print("{}: MathML postprocessing failed - timeout".format(filename))
         writesanitized(output,clean_file_name)
         return "{}: MathML postprocessing failed - timeout".format(filename)
+    # try:
+    #     proc = subprocess.Popen(["latexmlpost", "--format=html5", "--destination="+os.path.join(outdir,clean_file_name)+".html", "--mathimages", "-"],
+    #     stderr=PIPE, stdout=PIPE, stdin=PIPE)
+    #     stdout3, stderr = proc.communicate(stdout, timeout=timeout)
+    # except subprocess.TimeoutExpired:
+    #     proc.kill()
+    #     print("{}: MathML postprocessing failed - timeout".format(filename))
+    #     writesanitized(output,clean_file_name)
+    #     return "{}: MathML postprocessing failed - timeout".format(filename)
     if len(stdout2.strip())==0:
         print("{}: Conversion failed".format(filename))
         writesanitized(output,clean_file_name)
@@ -67,6 +85,8 @@ def genxhtml(filename):
     stdout2 = stdout2.decode()
     stdout2 = re.sub(r'(href\=\").*?(LaTeXML\.css)(\")',r'\1\2\3',stdout2)
     stdout2 = re.sub(r'(href=\").*?(ltx-article\.css)(\")',r'\1\2\3',stdout2)
+    with open(outrawname,'w') as fh:
+        fh.write(stdout.decode())
     with open(outfname,'w') as fh:
         fh.write(stdout2)
     return ""
@@ -76,16 +96,21 @@ def main():
     parser = argparse.ArgumentParser(description='Conversion of sanitized LaTeX documents to XHTML')
     parser.add_argument("directory",help="Path to directory of .tex files")
     parser.add_argument("xhtml_dir",help="Path to xhtml output directory")
+    parser.add_argument("--timeout",help="Specify custom timeout")
     args = parser.parse_args()
     origdir = os.getcwd()
     global path
     global outpath
     global erroroutputpath
+    global timeout
     path = args.directory
     outpath = args.xhtml_dir
     if not os.path.isdir(path):
         print("Error: {} is not a valid directory".format(path))
         sys.exit()
+    if args.timeout:
+        timeout = int(args.timeout)
+        print("New timeout: {}s".format(timeout))
     outpath = os.path.join(os.path.abspath(outpath),'')
     validate_folder(outpath)
     os.chdir(outpath)
