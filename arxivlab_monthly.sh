6e828cc3da3d0b2f1be36ef60bfdce0cee530e52
Daniel Jeong <daniel.jeong@columbia.edu>
2018-12-21 00:36:21 +0900

    Initial commit download & demacro scripts

---
+++ arxivlab_monthly.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download article data from remote mount path (UChicago) and demacro the articles
+python3 article_service.py -i ~/.ssh/id_rsa
+cp /local/hoptex/article_service_test/update_article_service/processed_articles/1812/1812_tex/* /local/hoptex/monthly_test/
+
+# Run enumerate.py on the new articles
+python3 enumerate.py /local/hoptex/monthly_test 10
